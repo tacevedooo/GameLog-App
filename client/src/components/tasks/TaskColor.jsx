6f59bb8edb6dcc4570169711d0c3cd6827e8547a
tacevedooo <tacevedo@unal.edu.co>
2026-02-10 17:48:06 -0500

    init

--- client/src/components/tasks/TaskColor.jsx
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx";
-import React from "react";
-
-const TaskColor = ({ className }) => {
-  return <div className={clsx("w-4 h-4 rounded-full", className)} />;
-};
-
-export default TaskColor;
--- client/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Dashboard from "./Dashboard";
-import Login from "./Login";
-import TaskDetail from "./TaskDetail";
-import Tasks from "./Tasks";
-import Trash from "./Trash";
-import Users from "./Users";
-import StatusPage from "./Status";
-
-export { Dashboard, Login, TaskDetail, Tasks, Trash, Users, StatusPage };
